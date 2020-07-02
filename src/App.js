adc4ea98c71066ef419b150efd7eaccc0d91a62e
Claudio <claudiosdv@gmail.com>
2020-07-02 20:32:22 +0100

    component completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import Logo from './images/logo.svg'
+import IconDocument from './images/icon-document.svg'
+import IconFolder from './images/icon-folder.svg'
+import IconUpload from './images/icon-upload.svg'
 import './App.css';
 
 function App() {
+
+  const [usage] = useState({
+    min:0,
+    max:1000,
+    used:815
+  })
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div id='data-storage-component'>
+      <div className='section two-fifth'>
+        <div className='content'>
+          <img src={Logo} alt='Fylo Logo' />
+          <div id='buttons'>
+            <div className='button'><img src={IconDocument} alt='Documents' /></div>
+            <div className='button'><img src={IconFolder} alt='Documents' /></div>
+            <label htmlFor='upload'><div className='button'><img src={IconUpload} alt='Documents' /></div></label><input type='file' id='upload' />
+          </div>
+        </div>
+      </div>
+      <div className='section three-fifth'>
+        <div className='content'>
+          <div id='space-left'><span>{usage.max - usage.used}</span> GB left</div>
+          <span className='description'>You've used <strong>{usage.used} GB</strong> of your storage</span>
+          <div className='progress-bar-outer'>
+            <div className='progress-bar-inner' style={{width:(usage.used * 100 / 1000) - 10 +'%'}}></div>
+          </div>
+          <div className='range'><span>{usage.min} GB</span><span>{usage.max} GB</span></div>
+        </div>
+      </div>
+      </div>
     </div>
   );
+
 }
 
 export default App;
